29ef79f4e4ef3a051f00e5a637e95c13328acc09
Stan Verlaan <s.verlaan@ziggo.nl>
2019-01-29 11:59:18 +0100

    Layout change csv
    Added a header and made viewed some really raw data

--- csv_creater.py
+++ csv_creater.py
@@ -6,5 +6,13 @@ outfile = open("file2.csv", "w")
 
 writer = csv.writer(outfile)
 
+#Write CSV header
+header = '{"s": "sendtime", "t": temperatuur", "p": pressure", "a": "alltitude"}'
+#f = csv.writer(["sendtime", "temperatuur", "pressure", "alltitude"])
+
+writer.writerow('ds') #De header
+
 for row in json.loads(infile.read()):
-    writer.writerow(row)
+    writer.writerow(row.values())
+
+print("Just completed your csv file!")
